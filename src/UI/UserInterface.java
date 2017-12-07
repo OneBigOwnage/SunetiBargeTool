9cb35fbfb2e0a53918d0e062c86278173fc1ec8f
OneBigOwnage <niekvandenbos@hotmail.com>
2017-12-07 14:57:31 +0100

    Sidebar click events are now onMousePressed instead of clicked/down

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -85,6 +85,7 @@ public class UserInterface extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new UserInterface().setVisible(true);
             }
