22832bae8c7f4053318061b72c87fa0dfb92875a
suneti <suneti@MSI.lan>
2018-02-07 17:16:32 +0100

    Make a start on Standard Procedures.

---
+++ src/StandardProcedures/ProcedureManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardProcedures;
+
+import sun.reflect.Reflection;
+
+/**
+ *
+ * @author suneti
+ */
+public class ProcedureManager {
+    
+    public static void executeProcedure() {
+        
+    }
+    
+    
+    
+    
+}
