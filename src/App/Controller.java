3f5197fdefeb063b85a88cc541cbf8493253af12
OneBigOwnage <niekvandenbos@hotmail.com>
2018-02-18 14:51:04 +0100

    Refactor all Config.View and Config.Model calls, to not include the word Config because it's not needed. Add getters for the Models and Views instantiated by the Controller.

--- src/App/Controller.java
+++ src/App/Controller.java
@@ -27,7 +27,6 @@ import java.util.TreeMap;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
-import sunetibargetool.Config;
 import sunetibargetool.Config.Model;
 import sunetibargetool.Config.View;
 
@@ -54,7 +53,7 @@ public class Controller {
      * Shows the showLoginView screen.
      */
     public void showLoginView() {
-        this.userInterface.setPanel(viewList.get(Config.View.LOGIN_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.LOGIN_VIEW), BorderLayout.CENTER);
     }
 
     /**
@@ -62,8 +61,8 @@ public class Controller {
      */
     public void afterLogin() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.SIDE_BAR), BorderLayout.WEST);
-        this.userInterface.setPanel(viewList.get(Config.View.BARGE_INFO_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.SIDE_BAR), BorderLayout.WEST);
+        this.userInterface.setPanel(viewList.get(View.BARGE_INFO_VIEW), BorderLayout.CENTER);
     }
 
     /**
@@ -71,7 +70,7 @@ public class Controller {
      */
     public void showBargeInfoView() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.BARGE_INFO_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.BARGE_INFO_VIEW), BorderLayout.CENTER);
     }
 
     /**
@@ -79,22 +78,22 @@ public class Controller {
      */
     public void showConfigView() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.CONFIG_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.CONFIG_VIEW), BorderLayout.CENTER);
     }
 
     public void showSQLView() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.SQL_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.SQL_VIEW), BorderLayout.CENTER);
     }
 
     public void showLogView() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.LOG_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.LOG_VIEW), BorderLayout.CENTER);
     }
 
     public void showStandardProcedureView() {
         this.userInterface.clearPanel(BorderLayout.CENTER);
-        this.userInterface.setPanel(viewList.get(Config.View.STANDARD_PROCEDURE_VIEW), BorderLayout.CENTER);
+        this.userInterface.setPanel(viewList.get(View.STANDARD_PROCEDURE_VIEW), BorderLayout.CENTER);
     }
 
     private void fixUserInterface() {
@@ -115,23 +114,45 @@ public class Controller {
     }
 
     private void initModels() {
-        modelList.put(Config.Model.BARGE_INFO_MODEL, new BargeInfoModel(this));
-        modelList.put(Config.Model.SQL_MODEL, new SQLModel(this));
-        modelList.put(Config.Model.PROCEDURE_MODEL, new ProcedureModel(this));
+        modelList.put(Model.BARGE_INFO_MODEL, new BargeInfoModel(this));
+        modelList.put(Model.SQL_MODEL, new SQLModel(this));
+        modelList.put(Model.PROCEDURE_MODEL, new ProcedureModel(this));
     }
 
     private void initViews() {
-        viewList.put(Config.View.LOG_VIEW, new LogView(this));
-        viewList.put(Config.View.BARGE_INFO_VIEW, new BargeInfoView(this, (BargeInfoModel) modelList.get(Config.Model.BARGE_INFO_MODEL)));
-        viewList.put(Config.View.LOGIN_VIEW, new LoginView(this));
-        viewList.put(Config.View.SQL_VIEW, new SQLView(this, (SQLModel) modelList.get(Config.Model.SQL_MODEL)));
-        viewList.put(Config.View.SIDE_BAR, new SideBar(this));
-        viewList.put(Config.View.CONFIG_VIEW, new ConfigView(this));
-        viewList.put(Config.View.STANDARD_PROCEDURE_VIEW, new StandardProcedureView(this, (ProcedureModel) modelList.get(Config.Model.PROCEDURE_MODEL)));
+        viewList.put(View.LOG_VIEW, new LogView(this));
+        viewList.put(View.BARGE_INFO_VIEW, new BargeInfoView(this, (BargeInfoModel) modelList.get(Model.BARGE_INFO_MODEL)));
+        viewList.put(View.LOGIN_VIEW, new LoginView(this));
+        viewList.put(View.SQL_VIEW, new SQLView(this, (SQLModel) modelList.get(Model.SQL_MODEL)));
+        viewList.put(View.SIDE_BAR, new SideBar(this));
+        viewList.put(View.CONFIG_VIEW, new ConfigView(this));
+        viewList.put(View.STANDARD_PROCEDURE_VIEW, new StandardProcedureView(this, (ProcedureModel) modelList.get(Model.PROCEDURE_MODEL)));
     }
 
     public void log(String text) {
-        ((LogView) viewList.get(Config.View.LOG_VIEW)).appendToLog(text);
+        ((LogView) viewList.get(View.LOG_VIEW)).appendToLog(text);
+    }
+
+    /**
+     * Retrieves a view by Config.View.
+     *
+     * @param viewName Name of the view you want to retrieve, in Config.View
+     * enum format.
+     * @return
+     */
+    public JPanel getView(View viewName) {
+        return viewList.get(viewName);
+    }
+
+    /**
+     * Retrieves a model by Config.Model.
+     *
+     * @param modelName Name of the model you want to retrieve, in Config.Model
+     * enum format.
+     * @return
+     */
+    public BaseModel getModel(Model modelName) {
+        return modelList.get(modelName);
     }
 
 }
