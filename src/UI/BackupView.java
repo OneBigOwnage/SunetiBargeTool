4eec78246a0c2514a4bfc3ec510e72da155c8409
OneBigOwnage <niekvandenbos@hotmail.com>
2018-02-19 23:36:55 +0100

    Interim commit.

--- src/UI/BackupView.java
+++ src/UI/BackupView.java
@@ -7,11 +7,17 @@ package UI;
 
 import App.Controller;
 import Models.BackupModel;
+import UiHelpers.UiLib;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import javax.swing.AbstractButton;
+import javax.swing.CellRendererPane;
+import javax.swing.JButton;
+import sunetibargetool.Config;
 
-/**
- *
- * @author suneti
- */
 public class BackupView extends javax.swing.JPanel {
 
     private final Controller controller;
@@ -39,24 +45,157 @@ public class BackupView extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        presetPanel = new javax.swing.JPanel();
+        loadPresetButton = new javax.swing.JButton();
+        presetPicker = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        backupPresetDescription = new javax.swing.JTextPane();
+        jLabel1 = new javax.swing.JLabel();
+        tablePanel = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(15, 124, 160));
+        setPreferredSize(new java.awt.Dimension(967, 479));
+
+        loadPresetButton.setText("Load Preset");
+        loadPresetButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                onLoadPresetButtonClicked(evt);
+            }
+        });
+
+        presetPicker.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "-- Choose a Backup Preset --", "Preset 01", "Preset 02" }));
+        presetPicker.setBorder(null);
+
+        jScrollPane1.setBorder(null);
+
+        backupPresetDescription.setEditable(false);
+        backupPresetDescription.setBorder(null);
+        jScrollPane1.setViewportView(backupPresetDescription);
+
+        jLabel1.setFont(UiLib.getTitleFont());
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Backup Presets");
+        jLabel1.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
+        javax.swing.GroupLayout presetPanelLayout = new javax.swing.GroupLayout(presetPanel);
+        presetPanel.setLayout(presetPanelLayout);
+        presetPanelLayout.setHorizontalGroup(
+            presetPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(presetPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(presetPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(loadPresetButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(presetPicker, 0, 273, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        presetPanelLayout.setVerticalGroup(
+            presetPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, presetPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addComponent(presetPicker, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(loadPresetButton)
+                .addContainerGap())
+        );
+
+        jButton1.setText("Create Backup");
+
+        javax.swing.GroupLayout tablePanelLayout = new javax.swing.GroupLayout(tablePanel);
+        tablePanel.setLayout(tablePanelLayout);
+        tablePanelLayout.setHorizontalGroup(
+            tablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, tablePanelLayout.createSequentialGroup()
+                .addContainerGap(489, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15))
+        );
+        tablePanelLayout.setVerticalGroup(
+            tablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(tablePanelLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(14, 14, 14))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 868, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(presetPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(tablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 481, Short.MAX_VALUE)
+            .addComponent(presetPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(tablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void onLoadPresetButtonClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_onLoadPresetButtonClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_onLoadPresetButtonClicked
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextPane backupPresetDescription;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton loadPresetButton;
+    private javax.swing.JPanel presetPanel;
+    private javax.swing.JComboBox<String> presetPicker;
+    private javax.swing.JPanel tablePanel;
     // End of variables declaration//GEN-END:variables
 
     /**
      * Method to do various fixes and style customizations to the GUI.
      */
     private void initComponentsExtra() {
+        // Set various backgrounds to the default color of this application.
+        this.setBackground(Config.Colors.APPLICATION_DEFAULT_BLUE.getColor());
+        tablePanel.setBackground(Config.Colors.APPLICATION_DEFAULT_BLUE.getColor());
+        presetPanel.setBackground(Config.Colors.APPLICATION_DEFAULT_BLUE.getColor());
+        backupPresetDescription.setBackground(Config.Colors.APPLICATION_DEFAULT_BLUE.getColor());
 
+        // Style the preset picker.
+//        Component[] components = presetPicker.getComponents();
+//        for (Component component : components) {
+//            if (component instanceof CellRendererPane) {
+//                // cell renderer here:
+//                CellRendererPane container = (CellRendererPane) component;
+//                System.out.println("Count: " + container.getComponentCount());
+//            } else {
+//                // JButton here:
+//                ((JButton) component).setBorder(UiLib.createDefaultLineBorder());
+//            }
+//        }
+        presetPicker.setBounds(new Rectangle(12, 9, 331, 26));
+        // Style the description pane.
+        UiLib.setComponentPadding(backupPresetDescription, 5, 5, 5, 5);
+        backupPresetDescription.setFont(UiLib.getDefaultFont());
+    }
+
+    /**
+     * Method to draw a separation line between the two main panels in this
+     * view.
+     *
+     * @param graphics
+     */
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        // Draw a line.
+        int margin = 0;
+        Graphics2D graphicsNew = (Graphics2D) graphics;
+        Line2D line = new Line2D.Float(presetPanel.getWidth(), 0 + margin, presetPanel.getWidth(), getHeight() - margin);
+        graphicsNew.draw(line);
     }
 }
