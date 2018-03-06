c9f326dc8f1267e8967e306d6d7e4d07eb02c2aa
suneti <suneti@MSI.lan>
2018-03-06 12:13:30 +0100

    Remove unused imports.

--- src/Models/BargeInfoModel.java
+++ src/Models/BargeInfoModel.java
@@ -14,13 +14,10 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
